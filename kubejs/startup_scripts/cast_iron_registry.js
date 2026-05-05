fa437315b4c745c3cf722f35e7f992277337ab5d
Jinxpurplerose <78373940+Jinxpurplerose@users.noreply.github.com>
2026-05-05 19:39:24 +0200

    repo cleaning

--- kubejs/startup_scripts/cast_iron_registry.js
+++
@@ -1,9 +0,0 @@
-StartupEvents.registry('item', event => {
-	event.create('cast_iron_billet')
-    	.displayName('Cast Iron Billet')
-    	.texture('kubejs:item/cast_iron_billet')
-    
-    event.create(`incomplete_cast_iron_billet`, 'create:sequenced_assembly')
-    	.displayName(`Incomplete Cast Iron Billet`)
-    	.texture(`kubejs:item/incomplete_cast_iron_billet`);
-})
