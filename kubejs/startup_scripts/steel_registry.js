52490ffb778a37a0b6c22e82642e1f7e6f6d8e31
Jinxpurplerose <78373940+Jinxpurplerose@users.noreply.github.com>
2026-05-05 19:41:30 +0200

    repo cleaning2

--- kubejs/startup_scripts/steel_registry.js
+++
@@ -1,9 +0,0 @@
-StartupEvents.registry('item', event => {
-	event.create('steel_billet')
-    	.displayName('Steel Billet')
-    	.texture('kubejs:item/steel_billet')
-    
-    event.create(`incomplete_steel_billet`, 'create:sequenced_assembly')
-    	.displayName(`Incomplete Steel Billet`)
-    	.texture(`kubejs:item/incomplete_steel_billet`);
-})
