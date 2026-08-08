fa259a2366535b840726a10f112ac82df81a8608
Jinxpurplerose <78373940+Jinxpurplerose@users.noreply.github.com>
2026-08-08 18:56:16 +0200

    feat: new steam engine recipe

---
+++ kubejs/startup_scripts/incomplete_steam_engine.js
@@ -0,0 +1,5 @@
+StartupEvents.registry('item', event => {
+    event.create('incomplete_steam_engine')
+        .displayName('Incomplete Steam Engine')
+        .texture('kubejs:item/incomplete_steam_engine')
+});
