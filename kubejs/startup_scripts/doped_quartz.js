086f108b9136b971dac328b552deb1e4b380d016
Jinxpurplerose <78373940+Jinxpurplerose@users.noreply.github.com>
2026-08-21 15:33:20 +0200

    fix&feat: tree physics fix and quartz compound added

---
+++ kubejs/startup_scripts/doped_quartz.js
@@ -0,0 +1,9 @@
+StartupEvents.registry('item', event => {
+    event.create('doped_quartz_alloy')
+        .displayName('Doped Quartz Alloy')
+        .texture('kubejs:item/doped_quartz_alloy')
+
+    event.create('doped_quartz_waffer')
+        .displayName('Doped Quartz Waffer')
+        .texture('kubejs:item/doped_quartz_waffer')
+});
